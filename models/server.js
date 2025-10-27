4e1bd3e12554fef22a812d2409a9b1e3a63992d7
Fernando Urbano <fernando_urbano@outlook.com>
2025-10-27 20:35:13 -0300

    Update server.js

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ import router from "../routes/user_routes.js";
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 3000;
 
     //middelewares
     this.middelewares();
